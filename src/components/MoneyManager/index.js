766c0da068704e98fe60bf328d9a0138df151d1f
Shaik Thabrej Basha <skthabrej@gmail.com>
2022-10-22 18:20:11 +0530

    money-manager

--- src/components/MoneyManager/index.js
+++ src/components/MoneyManager/index.js
@@ -1,3 +1,4 @@
+import { type } from '@testing-library/user-event/dist/type'
 import {Component} from 'react'
 import {v4 as uuidv4} from 'uuid'
 import MoneyDetails from '../MoneyDetails'
@@ -17,11 +18,75 @@ const transactionTypeOptions = [
 
 class MoneyManager extends Component {
 
-    state = {titleInput: '', amountInput: '', optionId: transactionTypeOptions[0].optionId, historyList: [],
-    totalBalance: 0, totalIncome: 0, totalExpenses: 0,}
+    state = {titleInput: '',
+      amountInput: '', 
+      optionId: transactionTypeOptions[0].optionId,
+      type:'Income',
+      historyList: [],
+      totalBalance: 0, 
+      totalIncome: 0,
+      totalExpenses: 0,
+    }
+
+    onDeleteItem = id => {
+        const {historyList} = this.state
+        const filteredUsersData = historyList.filter(
+          each => each.id !== id
+        )
+        this.setState({
+          historyList: filteredUsersData
+        })
+      }
+
+      addSubmitFrom = event => {
+        const{historyList} = this.state
+        event.preventDefault()
+        const{titleInput,amountInput,type} = this.state
+        const addNewDetails = {
+          title:titleInput,
+          amount:amountInput,
+          type:type,
+          id:uuidv4()
+        }
+        
+        this.setState(prevState => ({
+          historyList:[...prevState.historyList,addNewDetails],
+          titleInput:'',
+          amountInput:''
+        }))
+        let expenses = 0
+        let income = 0
+        let balance = 0
+        for (let item of historyList) {
+          if (item.title === 'income') {
+            income = income + parseInt(item.amount)
+          } else {
+            expenses = expenses + parseInt(item.amount)
+          }
+        }
+        balance = income - expenses
+        this.setState({
+          totalIncome:income,
+          totalBalance:balance,
+          totalExpenses:expenses
+        })
+      }
+
+
+      onTakeInputTitle = event => {
+        this.setState({titleInput:event.target.value})
+      }
+
+      onTakeInputAmount = event => {
+        this.setState({amountInput:event.target.value})
+      }
+
+      onTakeSelectType = event => {
+        this.setState({type:event.target.value})
+      }
 
     onGetHistoryLayout = () => {
-        const {titleInput, amountInput, optionId, historyList} = this.state
+        const {historyList} = this.state
         return (
           <div className="history-container">
             <h1>History</h1>
@@ -45,16 +110,17 @@ class MoneyManager extends Component {
       }
 
     onGetFormLayout = () => {
+      const {type,titleInput,amountInput} = this.state
         return (
-            <form className='form-container'>
+            <form className='form-container' onSubmit={this.addSubmitFrom}>
                 <div className='form-card-container'>
                     <h1 className='form-heading'>Add Transaction</h1>
                     <label className='label-1' htmlFor='title'>Title</label>
-                    <input type="text" id='title' className='all-input'></input>
+                    <input type="text" id='title' className='all-input' onChange={this.onTakeInputTitle} value={titleInput}></input>
                     <label className='label-1' htmlFor='amount'>Amount</label>
-                    <input type="text" id='amount'className='all-input'></input>
+                    <input type="text" id='amount'className='all-input' onChange={this.onTakeInputAmount} value={amountInput}></input>
                     <label className='label-1' htmlFor='select'>Type</label>
-                    <select id='select' className='select-input'>
+                    <select id='select' className='select-input' value={type} onChange={this.onTakeSelectType}>
                     {transactionTypeOptions.map(eachOption => (
                         <option
                             key={eachOption.optionId}
@@ -72,7 +138,9 @@ class MoneyManager extends Component {
     }
 
     render() {
-        const { titleInput,amountInput,optionId,historyList,totalBalance,totalIncome,totalExpenses,} = this.state
+        const { titleInput,amountInput,historyList,totalBalance,totalIncome,totalExpenses,type} = this.state
+        console.log(titleInput,amountInput,type)
+        console.log(historyList)
         return (
             <div className='bg-container'>
                 <div className='card-container'>
@@ -81,6 +149,9 @@ class MoneyManager extends Component {
                         <p className='main-paragraph'>Welcome back to your <span className='highlight-element'>!Money Manager</span></p>
                     </div>
                     <MoneyDetails
+                    totalBalance={totalBalance}
+                    totalExpenses={totalExpenses}
+                    totalIncome={totalIncome}
                     />
                     <div className="form-history-container">
                         {this.onGetFormLayout()}
--- src/components/TransactionItem/index.js
+++ src/components/TransactionItem/index.js
@@ -1,22 +1,26 @@
-import { Component } from "react";
 import './index.css'
 
 const deleteimg = "https://assets.ccbp.in/frontend/react-js/money-manager/delete.png"
 
-class TransactionItem extends Component {
-    render() {
-        const {historyItem} = this.props
-        const {title,amount,salary} = historyItem
-        return (
-            <div className="transcation-container">
-                <p className="details-all">{title}</p>
-                <p className="details-all">{amount}</p>
-                <p className="details-all">{salary}</p>
-                <button>
-                    <img src={deleteimg} className="delete-img" />
-                </button>
-            </div>
-        )
+const TransactionItem = props => {
+    const {historyItem,deleteFunction} = props
+    const {title,amount,salary,type,id} = historyItem
+
+    const onDeleteButton = () => {
+        console.log(id)
+        deleteFunction(id)
     }
+
+    return (
+        <div className="transcation-container">
+            <p className="details-all">{title}</p>
+            <p className="details-all">{amount}</p>
+            <p className="details-all">{type}</p>
+            <p className="details-all">{salary}</p>
+            <button className="btn-style" onClick={onDeleteButton}>
+                <img src={deleteimg} className="delete-img" />
+            </button>
+        </div>
+    )
 }
 export default TransactionItem
